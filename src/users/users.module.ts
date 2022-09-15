12d0cde4a1534376c7de7569ba41557acc3718d7
Palhanoo <bpalhano00@gmail.com>
2022-09-15 16:23:05 -0300

    save user in database

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Services } from '../utils/constants';
+import { User } from '../utils/typeorm';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [
     {
       provide: Services.USERS,
